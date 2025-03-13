7a3ad7d15b228f6c9dcad9cd65361e27234586bb
EwanSmith811 <thechosenone811@gmail.com>
2025-03-13 00:04:26 -0500

    changed file paths for npm commands

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // Must match your file name
+import App from './App.jsx' // Must match your file name
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
